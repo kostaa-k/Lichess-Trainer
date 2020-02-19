9940a3e43ae0d13deb7ee981caf6a2123d4b9967
Linda Zhang <31934414+lindazhang96@users.noreply.github.com>
2020-02-18 22:41:24 -0500

    Refactor

--- lichess-trainer-ui/src/components/ChessBoard.tsx
+++ lichess-trainer-ui/src/components/ChessBoard.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import CSS from 'csstype';
+import { SquareBoxType, PieceType } from './models';
+import { TileStyle } from './styles';
 import BlackKnight from '../pieces/black_knight.png';
 import BlackBishop from '../pieces/black_bishop.png';
 import BlackRook from '../pieces/black_rook.png';
@@ -12,72 +13,65 @@ import WhiteRook from '../pieces/white_rook.png';
 import WhiteKing from '../pieces/white_king.png';
 import WhiteQueen from '../pieces/white_queen.png';
 import WhitePawn from '../pieces/white_pawn.png';
+import defaultLineup from '../defaultLineup';
 
-const defaultLineup = require('../defaultLineup')
-
-const getDefaultLineup = () => defaultLineup.slice()
-
-type square_box = {
-    x_cord: Number,
-    y_cord: Number,
-    box_style: CSS.Properties,
-}
-
-type a_piece = {
-    unique_id: String,
-    current_square: square_box,
-    letter: String
-}
-
-const square = 100 / 8
-const squareSize = `${square}%`
-
-const box_style: CSS.Properties = {
-    width: squareSize,
-    height: squareSize,
-    paddingBottom: squareSize,
-    float: 'left',
-    position: 'relative',
-    pointerEvents: 'none',
-    alignItems: 'center',
-}
-
-let tiles: square_box[][];
-
-let piece_objects: a_piece[];
+let tiles: SquareBoxType[][];
+let currentPieces: PieceType[] = [];
 
 export class ChessBoard extends Component {
 
-    setup() {
+    isEven = (number: number) => {
+        return number % 2 == 0;
+    }
 
-        tiles = []
-
-        let count = 0
-
-        let temp_Square_tiles: square_box[];
-
+    tileInitialSetup = () => {
+        let tiles: SquareBoxType[][] = [];
+        let rowTiles: SquareBoxType[] = [];
+        let count = 0;
+   
         for (let y = 0; y < 8; y++) {
-            count = count + 1
+            const marginFromTop = y * 100 / 8;
+            count += 1
 
-            temp_Square_tiles = []
             for (let x = 0; x < 8; x++) {
-                //colour is white
-                let backgroundColor = '#f0d9b5'
+                let currentStyle = Object.assign({backgroundColor: '#f0d9b5', marginFromTop}, TileStyle); // colour is white
 
-                if (count % 2 == 0) {
-                    //colour is black
-                    backgroundColor = '#b58863'
+                if (this.isEven(count)) {
+                    currentStyle = Object.assign({backgroundColor: '#b58863', marginFromTop}, TileStyle) // colour is black
                 }
-                const top = y * square
-                const this_style = Object.assign({ backgroundColor, top }, box_style)
-                const temp_square = { x_cord: x, y_cord: y, box_style: this_style }
 
-                count = count + 1
-                temp_Square_tiles.push(temp_square)
+                rowTiles.push({ xCord: x, yCord: y, tileStyle: currentStyle })
+                count += 1
             }
 
-            tiles.push(temp_Square_tiles)
+            tiles.push(rowTiles)
         }
+    }
+
+    getDefaultLineup = () => defaultLineup.slice()
+
+    getStringPosition(letter: String, number: number): SquareBoxType {
+        const x_cord = letter.charCodeAt(0) - 97;
+        const y_cord = 8 - number;
+
+        return tiles[y_cord][x_cord]
+    }
+
+    initialize_pieces(): PieceType[] {
+        const defaultLineup = this.getDefaultLineup()
+        currentPieces = [];
+
+        defaultLineup.map(item => {
+            currentPieces.push({
+                uniqueId: "hello",
+                currentSquare: this.getStringPosition(
+                    item.split("@")[1].charAt(0),
+                    Number(item.split("@")[1].charAt(1))
+                ),
+                letter: item.split('@')[0],
+            })
+        });
+        return currentPieces;
     }
 
     get_piece_picture(letter: String, size: Number) {
@@ -112,48 +106,21 @@ export class ChessBoard extends Component {
         }
     }
 
-    decode_str_position(letter: String, the_num: number) {
-        // const input= "a7"
-
-        const x_cord = letter.charCodeAt(0) - 97;
-        const y_cord = 8 - the_num;
-
-        return tiles[y_cord][x_cord]
-    }
-
     componentWillMount() {
-        this.setup();
+        this.tileInitialSetup();
         this.initialize_pieces();
     }
 
-    initialize_pieces() {
-
-        piece_objects = []
-
-        const these_pieces = getDefaultLineup()
-
-        for (let i = 0; i < these_pieces.length; i++) {
-            const piece_str = these_pieces[i];
-            const position_str = piece_str.split(["@"])[1]
-            const the_letter = position_str.charAt(0);
-            const the_num_pos = Number(position_str.charAt(1));
-
-            const piece_name = piece_str.split(["@"])[0];
-            const piece_object = { unique_id: "hello", current_square: this.decode_str_position(the_letter, the_num_pos), letter: piece_name };
-
-            piece_objects.push(piece_object);
-        }
-    }
-
     render() {
+
         const all_tiles = tiles.map(tileRow => {
             const some_tiles = tileRow.map(tile => {
-                let listPieceInfo = piece_objects.filter(piece => piece.current_square.x_cord === tile.x_cord && piece.current_square.y_cord === tile.y_cord);
+                let listPieceInfo = currentPieces.filter(piece => piece.currentSquare.xCord === tile.xCord && piece.currentSquare.yCord === tile.yCord);
 
                 if (listPieceInfo[0] === undefined) {
-                    return <div style={tile.box_style}></div>
+                    return <div style={tile.tileStyle}></div>
                 } else {
-                    return <div style={tile.box_style}>{this.get_piece_picture(listPieceInfo[0].letter, 100)}</div>
+                    return <div style={tile.tileStyle}>{this.get_piece_picture(listPieceInfo[0].letter, 100)}</div>
                 }
             })
             return some_tiles;
